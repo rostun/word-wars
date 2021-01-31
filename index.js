75117a61457ea8f7c3c7c7645c40a179873fecec
rostun <rosa.m.tung@gmail.com>
2021-01-31 10:07:08 -0500

    put mat together

--- index.js
+++ index.js
@@ -1,89 +1,77 @@
-const HEX_SIDES = 6;
-const HEX_SIZE = 20;
-const HEX_PIECES = 5;
-const HEX_ROWS = 10;
-const ANGLE_30 = 30 * (180 / Math.PI);
-const RADIANS_30 = 30 * (Math.PI / 180);
-
 class Board {
     constructor(Context) {
         this.Context = Context;
+
+        this.HEX_SIDES = 6;
+        this.HEX_SIZE = 20;
+        this.ANGLE_30 = 30 * (180 / Math.PI);
+        this.START_X = 25;
+        this.START_Y = 25;
+        this.RADIANS_30 = 30 * (Math.PI / 180);
+
+        this.ROW_MAP = [
+            [0, 0, 1, 1, 1, 0, 0],
+            [0, 1, 1, 1, 1, 1, 0],
+            [0, 1, 1, 1, 1, 1, 0],
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+        ]
+
         Object.freeze(this);
     }
-    
+
     initialize() {
-        // hexagon
-        let info = { Xcenter: 25, Ycenter: 25 };
-    
-        for (let i = 0; i < HEX_ROWS; i++) {
-            this.drawRow(info);
-            
-            if(i%2 === 0) info.Xcenter = 59;
-            else info.Xcenter = 25;
-            
-            info.Ycenter += 20;
+        let Xcenter = this.START_X;
+        let Ycenter = this.START_Y;
+        for (let i = 0; i < this.ROW_MAP.length; i++) {
+            this.drawRow(Xcenter, Ycenter, this.ROW_MAP[i]);
+
+            Xcenter =  i % 2 === 0 ? this.START_X + 34 : this.START_X;
+
+            Ycenter += 20;
         }
     }
 
-    drawRow({ Xcenter, Ycenter }) {
-        for (let i = 0; i < HEX_PIECES; i++) {
-            this.drawHexPiece(Xcenter, Ycenter);
-            Xcenter += 67;
-        } 
-    }
+    drawRow(X, Y, piecesArray) {
 
-    polygon(X, Y) { 
-
-        this.Context.beginPath();
-        this.Context.moveTo (
-            X + HEX_SIZE * Math.cos(0), 
-            Y + HEX_SIZE *  Math.sin(0)
-        );
-    
-        for (let i = 1; i <= HEX_SIDES; i += 1) {
-            this.Context.lineTo (
-                X + HEX_SIZE * Math.cos(i * 2 * Math.PI / HEX_SIDES), 
-                Y + HEX_SIZE * Math.sin(i * 2 * Math.PI / HEX_SIDES)
-            );
+        for (let i = 0; i < piecesArray.length; i++) {
+            if(piecesArray[i]) this.drawPolygon(X, Y, { sides: this.HEX_SIDES, size: this.HEX_SIZE });
+            X += 69;
         }
-    
-        this.Context.strokeStyle = "#000000";
-        this.Context.lineWidth = 1;
-        this.Context.stroke();
     }
-    
-    //this.drawHexPiece(Xcenter, Ycenter, RADIANS_30);
-    drawHexPiece(X, Y, radians) {
+
+    drawPolygon(X, Y, { sides, size, radians }) {
         const _rotateBy = radians || 0;
-    
+        const _sides = sides || 6;
+        const _size = size || 20;
+
         this.Context.beginPath();
         this.Context.moveTo(
-            X + HEX_SIZE * Math.cos(0 + _rotateBy),
-            Y + HEX_SIZE * Math.sin(0 + _rotateBy)
+            X + _size * Math.cos(0 + _rotateBy),
+            Y + _size * Math.sin(0 + _rotateBy)
         );
-    
-        for (let i = 1; i <= HEX_SIDES; i += 1) {
+
+        for (let i = 1; i <= _sides; i += 1) {
             this.Context.lineTo(
-                X + HEX_SIZE * Math.cos((i * 2 * Math.PI / HEX_SIDES) + _rotateBy),
-                Y + HEX_SIZE * Math.sin((i * 2 * Math.PI / HEX_SIDES) + _rotateBy)
+                X + _size * Math.cos((i * 2 * Math.PI / _sides) + _rotateBy),
+                Y + _size * Math.sin((i * 2 * Math.PI / _sides) + _rotateBy)
             );
         }
-    
+
         this.Context.strokeStyle = "#000000";
         this.Context.lineWidth = 1;
         this.Context.stroke();
     }
-}
-
-function draw() {
-
-
 }
 
 window.onload = () => {
     const ctx = document.getElementById('board').getContext('2d');
     const GameBoard = new Board(ctx);
     GameBoard.initialize();
- };
+};
 
 //setInterval(draw, 10);
